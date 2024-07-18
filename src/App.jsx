4326e4ce739be3d6c1f1dc5f1d4dc84ac7dca4ed
baagena <baagena@gmail.com>
2024-07-18 15:40:11 +0200

    fetch data with react router 'Loader': pizza menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,43 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import Order from "./features/order/Order";
+import CreateOrder from "./features/order/CreateOrder";
+import AppLayout from "./ui/appLayout";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+        loader: menuLoader,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+      },
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+      },
+    ],
+  },
+]);
 function App() {
   // const x = 23;
 
-  return <div>Hello vite!</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/ui/Header.jsx
@@ -0,0 +1,7 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return <Link to="/">Fast React Pizza Co.</Link>;
+}
+
+export default Header;
