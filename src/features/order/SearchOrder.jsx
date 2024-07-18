5ed142edfad22f66c65fb5d2ce19a0b7291810ca
baagena <baagena@gmail.com>
2024-07-18 17:20:06 +0200

    Search order by id

---
+++ src/features/order/SearchOrder.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function SearchOrder() {
+  const [query, setQuery] = useState("");
+  const navigator = useNavigate();
+  function handleSubmit(e) {
+    e.preventDefault();
+    navigator(`/order/${query}`);
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        placeholder="Search order #"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+    </form>
+  );
+}
+
+export default SearchOrder;
